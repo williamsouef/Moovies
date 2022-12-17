38de5905eefb9757041eebe144fb0b63eb21070c
WILLIAM SOUEF <williamsouef@gmail.com>
2022-12-17 15:51:01 +0100

    Initial Commit

---
+++ Moovies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moovies
+//
+//  Created by William Souef on 17/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moovies/MooviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MooviesApp.swift
+//  Moovies
+//
+//  Created by William Souef on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MooviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
