dfbc8dd9c148ccded2ee2514d2b607da72177355
WILLIAM SOUEF <williamsouef@gmail.com>
2022-12-21 10:50:07 +0100

    Adding the closingButton and changing the Stacks

--- Moovies/OnboardingView.swift
+++ Moovies/OnboardingView.swift
@@ -18,12 +18,24 @@ struct OnboardingView: View {
                     .edgesIgnoringSafeArea(.all)
                     
             VStack{
+                VStack{
             TitleApp()
+                      
                 Divider()
-                
+                    VStack(alignment: .center, spacing: 30){
             InfoView(text: "Find the best classic movies")
             InfoView(text: "Buy online and get promotion")
             InfoView(text: "Watch in premiere and enjoy !")
+                
+                    }
+                    .padding(.vertical,70)
+                }
+                .padding(.vertical)
+                Button(action: {
+                    onboardingIsShowing = false
+                },label: {
+                    RoundCloseViews()
+                } )
                     
             }
         }
--- Moovies/Views/InfoButton.swift
+++ Moovies/Views/InfoButton.swift
@@ -21,10 +21,27 @@ struct InfoButton: View {
     )
     }
 }
+struct RoundCloseViews: View {
+ 
+    var body: some View {
+        Image(systemName: "xmark")
+            .font(.title)
+            .foregroundColor(Color.yellow)
+            .frame(width: 100.0, height: 56.0)
+            .background(
+                Circle()
+                    .fill(Color.black)
+                .transition(.scale)
+                
+            )
+            .shadow(color: .yellow, radius: 5, x: 0, y: 0)
+    }
+}
 
 struct InfoButton_Previews: PreviewProvider {
     static var previews: some View {
         InfoButton()
             .preferredColorScheme(.dark)
+        RoundCloseViews()
     }
 }
